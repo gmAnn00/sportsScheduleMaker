9cdef9f7dafbe0424c21a50b52b0d4f891c7c9ae
gmAnn00 <bu9458@gmail.com>
2023-03-04 20:53:32 +0900

    index 수정, 슬라이드 배너 작성

---
+++ js/index.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+  document.querySelector(".imgBtn1").addEventListener("click", function () {
+    document.querySelector(".bannerContainer").style.transform =
+      "translate(0px)";
+  });
+  document.querySelector(".imgBtn2").addEventListener("click", function () {
+    document.querySelector(".bannerContainer").style.transform =
+      "translate(-250px)";
+  });
+  document.querySelector(".imgBtn3").addEventListener("click", function () {
+    document.querySelector(".bannerContainer").style.transform =
+      "translate(-500px)";
+  });
+};
