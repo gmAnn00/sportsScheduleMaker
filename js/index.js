9d24d093199d2d95489538a6c740b47238f560f8
gmAnn00 <bu9458@gmail.com>
2023-03-07 21:33:53 +0900

    css 다듬었음

--- js/index.js
+++ js/index.js
@@ -1,14 +1,11 @@
-window.onload = function () {
-  document.querySelector(".imgBtn1").addEventListener("click", function () {
-    document.querySelector(".bannerContainer").style.transform =
-      "translate(0px)";
-  });
-  document.querySelector(".imgBtn2").addEventListener("click", function () {
-    document.querySelector(".bannerContainer").style.transform =
-      "translate(-250px)";
-  });
-  document.querySelector(".imgBtn3").addEventListener("click", function () {
-    document.querySelector(".bannerContainer").style.transform =
-      "translate(-500px)";
-  });
-};
+document.querySelector(".imgBtn1").addEventListener("click", function () {
+  document.querySelector(".bannerContainer").style.transform = "translate(0px)";
+});
+document.querySelector(".imgBtn2").addEventListener("click", function () {
+  document.querySelector(".bannerContainer").style.transform =
+    "translate(-250px)";
+});
+document.querySelector(".imgBtn3").addEventListener("click", function () {
+  document.querySelector(".bannerContainer").style.transform =
+    "translate(-500px)";
+});
